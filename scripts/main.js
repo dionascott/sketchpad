c29bf9434512b41c8d4187d35b55c5168c3cab22
Dion Scott <diontscott@gmail.com>
2017-07-29 11:05:24 -0500

    The sketchpad now displays random colors when hovered over.

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,52 @@
 
 $(document).ready(function(){
-  var boxes = $('.container').find('.boxes');
-  var inputNumber = 10;
-  var boxNumber = (inputNumber * inputNumber)
-  var boxSize = (600/inputNumber)
-  for(var i = 0;i < boxNumber; i++){
-  $('.container').append("<div class='boxes'></div>");
-  $('.container').find('.boxes').height(boxSize).width(boxSize)
+  /*var box = $('#container').find('.boxes');
+  var container = $('#container')*/
+  var inputNumber = 2;
+  var boxNumber = (inputNumber * inputNumber);
+  var boxSize = (600/inputNumber);
+  var boxes = '';
+  var i = 0;
+
+  function getBoxes(){
+  while(i < boxNumber){
+  boxes += "<div class='boxes'></div>";
+  i++;
+  };
+
+  $('#container').html(boxes);
+  $('#container').find('.boxes').height(boxSize).width(boxSize)
   .addClass('box');
   };
-  $('.container').find('.boxes').on('mouseenter', function(){
-    $(this).addClass('lighter')
-  });
 
-  $('.container').find('.boxes').on('mouseleave', function(){
-    $(this).removeClass('lighter').toggleClass('darker');
+  function findColor() {
+    var color = 'rgb(' + (Math.floor(Math.random() * 256)) + ','
+                      + (Math.floor(Math.random() * 256)) + ','
+                      + (Math.floor(Math.random() * 256)) + ')';
+                      return color;
+  };
+
+  function addEffect() {
+    $('.box').on('mouseenter', function(){
+      randColor = findColor();
+      $(this).css({'background-color': randColor}, 200);
+      });
+      };
+
+
+
+  getBoxes();
+  addEffect();
+  $('#button').on('click', '#reset', function() {
+    $('.box').detach();
+    //Maybe...idk makes too many boxes
+    var userInput = prompt("Please enter a number", "8");
+    inputNumber = parseInt(userInput);
+    boxNumber = (inputNumber * inputNumber);
+    boxSize = (600/inputNumber);
+    boxes = '';
+    i = 0;
+    getBoxes();
+    addEffect();
   });
 });
